4ed112618ff715b094716a921035f609837536bf
seiko <seikoseikou.com@gmail.com>
2019-12-20 20:10:32 +0900

    プロジェクトを作成

---
+++ UnEnableTableViewCellDemo/UnEnableTableViewCellDemo/InitialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitialViewController.swift
+//  UnEnableTableViewCellDemo
+//
+//  Created by 石場清子 on 2019/12/20.
+//  Copyright © 2019 石場清子. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
